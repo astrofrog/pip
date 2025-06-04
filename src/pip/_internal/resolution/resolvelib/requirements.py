78a1a6fb954ac2efccf0566b064755ca64720b34
Thomas Robitaille <thomas.robitaille@gmail.com>
2025-06-04 15:01:12 +0100

    Fix

--- src/pip/_internal/resolution/resolvelib/requirements.py
+++ src/pip/_internal/resolution/resolvelib/requirements.py
@@ -130,7 +130,7 @@ class SpecifierWithoutExtrasRequirement(SpecifierRequirement):
         print('SpecifierWithoutExtrasRequirement.__init__')
         print(ireq.extras)
         self._ireq = install_req_drop_extras(ireq)
-        if EXPLICIT_EMPTY_EXTRAS in ireq.extras:
+        if ireq.extras:
             self._ired.extras = {EXPLICIT_EMPTY_EXTRAS}
         print(self._ireq.extras)
         self._equal_cache: Optional[str] = None
